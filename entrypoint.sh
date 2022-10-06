4ceaf5a9cad7a74a43a18c9283e373bacb7ef560
Safi <saphidev@gmail.com>
2022-10-06 19:40:17 +0200

    new action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-set -eu
-
 export GITHUB="true"
+
+set +u
+
+export INPUT_SCRIPT=$(cat <<-END
+    docker stop ${INPUT_CONTAINERNAME}_container
+    docker rm ${INPUT_CONTAINERNAME}_container
+    docker rmi -f ${INPUT_CONTAINERNAME}_image
+    cd /home/${INPUT_CONTAINERNAME}
+    docker build -t ${INPUT_CONTAINERNAME}_image .
+    docker run -d -p ${INPUT_CONTAINER_PORT}:${INPUT_CONTAINER_PORT} --restart=always --name=${INPUT_CONTAINERNAME}_container ${INPUT_CONTAINERNAME}_image
+END
+)
 
 sh -c "/bin/drone-ssh $*"
