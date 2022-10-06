3b38c6f154cbfd700d3105b41c7caf13efdcd015
Safi <saphidev@gmail.com>
2022-10-06 19:55:26 +0200

    fix entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ export GITHUB="true"
 set +u
 
 export INPUT_SCRIPT=$(cat <<-END
-    docker stop ${INPUT_CONTAINERNAME}_container
-    docker rm ${INPUT_CONTAINERNAME}_container
-    docker rmi -f ${INPUT_CONTAINERNAME}_image
-    cd /home/${INPUT_CONTAINERNAME}
-    docker build -t ${INPUT_CONTAINERNAME}_image .
-    docker run -d -p ${INPUT_CONTAINER_PORT}:${INPUT_CONTAINER_PORT} --restart=always --name=${INPUT_CONTAINERNAME}_container ${INPUT_CONTAINERNAME}_image
+    docker stop ${INPUT_CONTAINER_NAME}_container
+    docker rm ${INPUT_CONTAINER_NAME}_container
+    docker rmi -f ${INPUT_CONTAINER_NAME}_image
+    cd /home/${INPUT_CONTAINER_NAME}
+    docker build -t ${INPUT_CONTAINER_NAME}_image .
+    docker run -d -p ${INPUT_CONTAINER_PORT}:${INPUT_CONTAINER_PORT} --restart=always --name=${INPUT_CONTAINER_NAME}_container ${INPUT_CONTAINER_NAME}_image
 END
 )
 
